babaf50dd2c04b9a9c5417a229e56fc0826c1c56
EC2 Default User <ec2-user@ip-172-31-54-42.ec2.internal>
2021-04-08 08:43:26 +0000

    Change Todo status

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -16,11 +16,21 @@ import Filter from './Filter';
 import useStorage from '../hooks/storage';
 
 /* ライブラリ */
-import {getKey} from "../lib/util";
+import { getKey } from "../lib/util";
 
 function Todo() {
+  const handleCheck = checked => {
+    const newItems = items.map(item => {
+      if (item.key === checked.key) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    putItems(newItems);
+  };
+
   const [items, putItems] = React.useState([
-      /* テストコード 開始 */
+    /* テストコード 開始 */
     { key: getKey(), text: '日本語の宿題', done: false },
     { key: getKey(), text: 'reactを勉強する', done: false },
     { key: getKey(), text: '明日の準備をする', done: false },
@@ -36,6 +46,7 @@ function Todo() {
         <TodoItem
           key={item.key}
           item={item}
+          onCheck={handleCheck}
         />
       ))}
       <div className="panel-block">
