206c97a01bb5c506e83dcd0b221a29777c394e1f
EC2 Default User <ec2-user@ip-172-31-54-42.ec2.internal>
2021-04-11 07:46:25 +0000

    Save Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,6 +19,16 @@ import useStorage from '../hooks/storage';
 import { getKey } from "../lib/util";
 
 function Todo() {
+  const [items, putItems, clearItems] = useStorage();
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+
+  const [filter, setFilter] = React.useState('ALL');
+
   const handleCheck = checked => {
     const newItems = items.map(item => {
       if (item.key === checked.key) {
@@ -34,22 +44,6 @@ function Todo() {
   };
 
   const handleFilterChange = value => setFilter(value);
-
-  const [filter, setFilter] = React.useState('ALL');
-
-  const [items, putItems] = React.useState([
-    /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
-
-  const displayItems = items.filter(item => {
-    if (filter === 'ALL') return true;
-    if (filter === 'TODO') return !item.done;
-    if (filter === 'DONE') return item.done;
-  });
 
   return (
     <div className="panel">
@@ -70,6 +64,11 @@ function Todo() {
       ))}
       <div className="panel-block">
         {displayItems.length} items
+      </div>
+      <div className="panel-block">
+        <button className="button is-light is-fullwidth" onClick={clearItems}>
+          全てのToDoを削除
+        </button>
       </div>
     </div>
   );
--- src/hooks/storage.js
+++ src/hooks/storage.js
@@ -1,3 +1,4 @@
+/*global localStorage*/
 import { useState, useEffect } from 'react';
 
 /* 
@@ -12,19 +13,26 @@ import { useState, useEffect } from 'react';
 const STORAGE_KEY = 'itss-todo';
 
 function useStorage() {
-  const [items, setItems] = useState([]);
-　
-　/* 副作用を使う */
+  const [items, setItems] = useState([]);　　 /* 副作用を使う */
   useEffect(() => {
-    
+    const data = localStorage.getItem(STORAGE_KEY);
+
+    if (!data) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+    }
+    else {
+      setItems(JSON.parse(data));
+    }
   }, []);
 
   const putItems = items => {
-    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+    setItems(items);
   };
 
   const clearItems = () => {
-    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+    setItems([]);
   };
 
   return [items, putItems, clearItems];
